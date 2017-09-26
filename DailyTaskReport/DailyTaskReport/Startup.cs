8e580b52940d1fab2215d925d1ce31f0f2f48605
kheproject <31459894+kheproject@users.noreply.github.com>
2017-09-26 11:19:07 +0800

    Remade

---
+++ DailyTaskReport/DailyTaskReport/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DailyTaskReport.Startup))]
+namespace DailyTaskReport
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
