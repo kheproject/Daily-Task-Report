dd59a39e4bae4a9adf9bba730affe455e54564d2
Khevin <venzentsanaga@gmail.com>
2017-10-06 22:14:19 +0800

    Updated

--- DailyTaskReport/DailyTaskReport/Controllers/TaskController.cs
+++ DailyTaskReport/DailyTaskReport/Controllers/TaskController.cs
@@ -27,8 +27,77 @@ namespace DailyTaskReport.Controllers
             connection = config["server"].ToString();
         }
 
+        public ActionResult getTask()
+        {
+            user_tasks data = new user_tasks();
+            List<daily_task> daily = new List<daily_task>();
+            List<task_list> tasks = new List<task_list>();
+            
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(connection))
+                {
+                    con.Open();
+                    using (MySqlCommand cmd = con.CreateCommand())
+                    {
+                        String dateMonth = getServerMonth();
+                        dateMonth = dateMonth.Length < 2 ? "0" + dateMonth : dateMonth;
+
+                        cmd.CommandText = "SELECT * FROM kpDailyTask.Report" + dateMonth
+                                        + " WHERE user = @user ORDER BY date, timeFrom DESC;";
+                        cmd.Parameters.AddWithValue("user", encdata.AESDecrypt(Session["_user"].ToString().Replace(' ', '+'), encStringKey));
+                        MySqlDataReader rdr = cmd.ExecuteReader();
+                        if (rdr.HasRows)
+                        {
+                            String currDate = string.Empty;
+                            while(rdr.Read())
+                            {
+                                if (currDate == string.Empty)
+                                    currDate = rdr["date"].ToString();
+                                if (currDate == rdr["date"].ToString())
+                                {
+                                    tasks.Add(new task_list
+                                    {
+                                        timeFrom = rdr["timeFrom"].ToString(),
+                                        timeTo = rdr["timeTo"].ToString(),
+                                        task = rdr["Task"].ToString(),
+                                        woNo = rdr["WOno"].ToString()
+                                    });
+                                }
+                                else
+                                {
+                                    daily.Add(new daily_task
+                                    {
+                                        task_date = Convert.ToDateTime(currDate),
+                                        taskLists = tasks
+                                    });
+                                    tasks = new List<task_list>();
+                                    currDate = string.Empty;
+                                }
+                            }
+
+                            //final daily add from last date result
+                            daily.Add(new daily_task
+                            {
+                                task_date = Convert.ToDateTime(currDate),
+                                taskLists = tasks
+                            });
+
+                            data = new user_tasks { user = encdata.AESDecrypt(Session["_user"].ToString().Replace(' ', '+'), encStringKey), tasks = daily };
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+            return PartialView(data);
+        }
+
         public ActionResult addTask()
         {
+
             return PartialView();
         }
         
@@ -49,6 +118,35 @@ namespace DailyTaskReport.Controllers
             return new JsonResult { Data = response };
         }
 
+
+        private String getServerMonth()
+        {
+            String date = string.Empty;
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(connection))
+                {
+                    con.Open();
+                    using (MySqlCommand cmd = con.CreateCommand())
+                    {
+                        cmd.CommandText = "SELECT MONTH(DATE_ADD(NOW(), INTERVAL 15 HOUR )) AS \"current_date\";";
+                        MySqlDataReader rdr = cmd.ExecuteReader();
+
+                        if (rdr.HasRows)
+                        {
+                            rdr.Read();
+                            date = rdr["current_date"].ToString();
+                            rdr.Close();
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                date = "error";
+            }
+            return date;
+        }
         //private TaskResponse addUserTasks(TaskLists tasks)
         //{
         //    TaskResponse response = new TaskResponse();
--- DailyTaskReport/DailyTaskReport/Scripts/kheProject.js
+++ DailyTaskReport/DailyTaskReport/Scripts/kheProject.js
@@ -52,6 +52,11 @@ function isLetterPress(e) { //for lowercase letters, isLetter is uppercase
     return e.keyCode >= 97 && e.keyCode <= 122 ? true : false;
 }
 
+function promptSessionExpired() {
+    console.log("Session expired.");
+    promptMsg("Current session has expired. <br /> Redirect to login page", true);
+}
+
 function addTaskForm() {
     if ($('#body-content').html() == "") {
         toggleUIBlocker();
@@ -67,8 +72,40 @@ function addTaskForm() {
                     if (result != "") {
                         $('#body-content').html(result);
                     }
-                    else
-                        window.location.href = "../";
+                    else {
+                        promptSessionExpired();
+                    }
+                }, 300);
+            },
+            error: function () {
+                toggleUIBlocker();
+                alert('an error has occured');
+            }
+        })
+    }
+    else {
+        $('#body-content').html("");
+    }
+}
+
+function getTaskView() {
+    if ($('#body-content').html() == "") {
+        toggleUIBlocker();
+        $.ajax({
+            async: true,
+            type: 'GET',
+            url: '/Task/getTask',
+            contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+            dataType: 'html',
+            cache: false,
+            success: function (result) {
+                setTimeout(function () {
+                    if (result != "") {
+                        $('#body-content').html(result);
+                    }
+                    else {
+                        promptSessionExpired();
+                    }
                 }, 300);
             },
             error: function () {
