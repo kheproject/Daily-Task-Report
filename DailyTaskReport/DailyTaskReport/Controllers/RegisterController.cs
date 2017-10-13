fdc49a9c25ad4b1b5bae8acdf7d20a662e4739a3
Khevin <venzentsanaga@gmail.com>
2017-10-14 07:29:14 +0800

    Update Register

--- DailyTaskReport/DailyTaskReport/Controllers/RegisterController.cs
+++ DailyTaskReport/DailyTaskReport/Controllers/RegisterController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using DailyTaskReport.Models;
 namespace DailyTaskReport.Controllers
 {
     public class RegisterController : Controller
@@ -12,6 +12,16 @@ namespace DailyTaskReport.Controllers
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(AccountModel user)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View("Index");
         }
     }
 }
